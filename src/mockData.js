752db867e505d5de1a3d426c3a16c1c3b40a9126
cou201 <Davidestradag200@gmail.com>
2022-07-06 12:03:18 -0500

    Ya

--- src/mockData.js
+++ src/mockData.js
@@ -4,13 +4,28 @@ import spotify from './images/spotify.png';
 import peliculas from "./images/Peliculas.jpg";
 import Pagina from "./images/Sin.png";
 import RedSocial from "./images/RedSocial.png";
-import commerce from "./images/Ecommerce.png";
+import commerce from "./images/commerce.png";
 
 const mockData = [
+  {
+    title: "RedSocial",
+    image: RedSocial,
+    link: "https://social-media-eight-alpha.vercel.app"
+  },
+  {
+    title: "E-commerce",
+    image:  commerce,
+    link: "https://e-commerce-liard.vercel.app/"
+  },
     {
         title: 'Rick & Morty',
          image: rickandmortywiki,
         link: 'https://rick-and-morty-app-jcmateus.netlify.app',
+    },
+    {
+      title: "Portafolio de peliculas",
+      image: peliculas,
+      link: "https://regal-moonbeam-94613e.netlify.app"
     },
     {
         title: 'weather app',
@@ -18,23 +33,9 @@ const mockData = [
         link: 'https://weather-search-react.netlify.app',
     },
     {
-      title: "Portafolio de peliculas",
-      image: peliculas,
-      link: "https://regal-moonbeam-94613e.netlify.app"
-    },{
       title: "Pagina Prueba de vista",
       image: Pagina,
       link: "https://guileless-custard-27db64.netlify.app"
-    },
-    {
-      title: "RedSocial",
-      image: RedSocial,
-      link: "https://social-media-eight-alpha.vercel.app"
-    },
-    {
-      title: "E-commerce",
-      image:  commerce,
-      link: "https://e-commerce-liard.vercel.app/"
     },
     {
       title: "Spotify image",
